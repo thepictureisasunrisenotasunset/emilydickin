af4fd4caebf14a227e12ce91e93a741ebf60495c
Benjamin Anderson <benanderson6108@gmail.com>
2019-07-23 15:13:12 -0600

    added ggplot2 graphs

--- emily_dickinson_work.R
+++ emily_dickinson_work.R
@@ -1,5 +1,6 @@
 library(tidyverse)
 
+
 # importing and initializing data ------------------------------
 emily <- read_file("emily_dickinson_titleless.txt")
 str(emily) # a character vector, 1 entry
@@ -82,16 +83,106 @@ ED # the final data frame, now in a representational format that is easy to work
 # Exploring the Data -------------------------------------------
 View(ED)
 
-# the number of poems written is impressive, but doesn't tell me much- I have no clue whether this file of poems is exhaustive for ED or not (I doubt it)
+# the number of poems (n = 356) written is impressive, but doesn't tell us much- I have no
+# clue whether this file of poems is exhaustive for ED or not (I doubt it)
 
-lpp <- ED %>% group_by(poem) %>% summarize("_lines_" = n()) # a tibble of number of lines per poem
-hist(lpp$`_lines_`) # a histogram
-boxplot(lpp$`_lines_`) # a boxplot
+# we can look at:
+# (1) stanzas per poem
+# (2) lines per poem
+# (3) lines per stanza
+# (4) finally, from the content and structure of the words used (this is NLP)
 
-spp <- ED %>% group_by(poem) %>% summarize("_stanzas_" = max(stanza)) # tibble of number of stanzas per poem
-hist(spp$`_stanzas_`) # a histogram
-boxplot(spp$`_stanzas_`) # a boxplot
+# (1)
+spp <- ED %>% group_by(poem) %>% summarize(S = max(stanza)) # tibble of number of stanzas per poem
+hist(spp$S) # a histogram
+boxplot(spp$S) # a boxplot
+
+# (2)
+lpp <- ED %>% group_by(poem) %>% summarize(L = n()) # a tibble of number of lines per poem
+hist(lpp$L) # a histogram
+boxplot(lpp$L) # a boxplot
+
+# (3)
+lps <- ED %>% group_by(poem, stanza) %>% summarize(L = n())
+hist(lps$L) # a histogram
+boxplot(lps$L) # a boxplot
+
+
+# We can make these graphs nicer by using ggplot2:
+
+# histogram of stanzas per poem
+moder <- function(x) {
+  as.double(names(sort(table(x))[length(table(x))]))
+} # this defines a mode function since mode() has a different meaning in base R
+
+ggplot(spp) +
+  geom_histogram(mapping = aes(spp$S), bins = max(spp$S)) +
+  labs(title = c("Distribution of stanzas per poem"), subtitle = paste("mean: ", round(mean(spp$S), digits = 2), " ; median: ", median(spp$S), " ; mode: ", moder(spp$S)), x = "stanzas per poem") +
+  scale_x_continuous(breaks = 1:12)
+
+# boxplot of stanzas per poem
+is.outlier <- function(x) {
+  a <- (x < quantile(x, 0.25) - 1.5 * IQR(x) | x > quantile(x, 0.75) + 1.5 * IQR(x))
+  return(a)
+} # returns a logical vector with TRUE cooresponding to the indices of outliers in x
+spp <- spp %>% mutate(outliers = ifelse(is.outlier(S), poem, NA)) # creates a column of outliers and NAs that can be usefully passed to ggplot
+# NOTE: this method inspired by user JasonAizkalns on https://stackoverflow.com/questions/33524669/labeling-outliers-of-boxplots-in-r
+library(ggrepel) # we need this package to use the geom_label_repel() function
+spp %>% filter(!(is.na(outliers))) %>% dim() %>% .[1] # there are 8 outliers
+
+ggplot(spp) +
+  geom_boxplot(mapping = aes(x = "", y = spp$S), outlier.color = "red") +
+  scale_y_continuous(breaks = 2*(1:6)) +
+  labs(x = "", y = "count", title = c("Boxplot distribution of stanzas per poem")) +
+  geom_label_repel(mapping = aes(x = "", y = spp$S, label = outliers), na.rm = TRUE, min.segment.length = 0)
+
+
+
+# histogram of lines per poem
+ggplot(lpp) +
+  geom_histogram(mapping = aes(lpp$L), bins = max(lpp$L)) +
+  labs(title = c("Distribution of lines per poem"), subtitle = paste("mean: ", round(mean(lpp$L), digits = 2), " ; median: ", median(lpp$L), " ; mode: ", moder(lpp$L)), x = "lines per poem") +
+  scale_x_continuous(breaks = 2*(1:25))
+
+# boxplot of lines per poem
+lpp <- lpp %>% mutate(outliers = ifelse(is.outlier(L), poem, NA))
+lpp %>% filter(!(is.na(outliers))) %>% dim() %>% .[1] # there are 8 outliers
+
+ggplot(lpp) +
+  geom_boxplot(mapping = aes(x = "", y = lpp$L), outlier.color = "red") +
+  scale_y_continuous(breaks = 2*(1:25)) +
+  labs(x = "", y = "count", title = c("Boxplot distribution of lines per poem")) +
+  geom_label_repel(mapping = aes(x = "", y = lpp$L, label = outliers), na.rm = TRUE, min.segment.length = 0)
+
+
+
+# histogram of lines per stanza
+ggplot(lps) +
+  geom_histogram(mapping = aes(lps$L), bins = max(lps$L)) +
+  labs(title = c("Distribution of lines per stanza"), subtitle = paste("mean: ", round(mean(lps$L), digits = 2), " ; median: ", median(lps$L), " ; mode: ", moder(lps$L)), x = "lines per stanza") +
+  scale_x_continuous(breaks = 1:16)
+
+# boxplot of lines per stanza
+lps <- ungroup(lps) %>% mutate(outliers = ifelse(is.outlier(lps$L), paste("p", poem, ",", "s", stanza), NA_character_))
+lps %>% filter(!(is.na(outliers))) %>% dim() %>% .[1]
+# there are 222 outliers here, so annotating the outliers of this graph will not help
+
+ggplot(lps) +
+  geom_boxplot(mapping = aes(x = "", y = lps$L), outlier.color = "red") +
+  scale_y_continuous(breaks = 1:16) +
+  labs(x = "", y = "count", title = c("Boxplot distribution of lines per stanza"))
+
+
+
+
+
+
+
+
+
 
 # add graphics
-# add exhaustive analysis of combinations of variables and significance
 # add analysis of possible significance of outliers
+# possible NLP analysis in the future
+# initial conclusions --------------------
+# 
